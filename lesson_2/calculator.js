83c1b2b8f4a2b7d2218c56dc08d894f60b96e4c4
colbyjaxcodes <colbyjaxcodes@gmail.com>
2023-08-12 18:01:51 -0700

    calculator app refactor/validation added. Add ESLint

--- lesson_2/calculator.js
+++ lesson_2/calculator.js
@@ -1,24 +1,61 @@
 const readline = require('readline-sync');
 
+function prompt(message) {
+  console.log(`=> ${message}`);
+}
+
+function invalidNumber(number) {
+  return number.trimStart() === '' || Number.isNaN(Number(number));
+}
+
+prompt('Welcome to Calculator!');
+
 // Ask the user for the first number.
-const firstNum = readline.question('Please enter your first number');
+prompt("What's the first number?");
+let firstNum = readline.question();
+// Number validation
+while (invalidNumber(firstNum)) {
+  prompt("Hmm... that doesn't look like a valid number.");
+  firstNum = readline.question();
+}
+
 // Ask the user for the second number.
-const secondNum = readline.question('Please enter your second number');
+prompt("What's the second number?");
+let secondNum = readline.question();
+
+while (invalidNumber(secondNum)) {
+  prompt("Hmm... that doesn't look like a valid number.");
+  secondNum = readline.question();
+}
+
 // Ask the user for an operation to perform.
-const operation = readline.question(
+prompt(
   'What operation would you like to perform?\n+) Add -) Subtract *) Multiply /) Divide'
 );
+let operation = readline.question();
+
+while (!['+', '-', '*', '/'].includes(operation)) {
+  prompt('Must choose +, -, * or /');
+  operation = readline.question();
+}
 
 // Perform the operation on the two numbers.
-if (operation === '+') {
-  answer = Number(firstNum) + Number(secondNum);
-} else if (operation === '-') {
-  answer = Number(firstNum) - Number(secondNum);
-} else if (operation === '*') {
-  answer = Number(firstNum) * Number(secondNum);
-} else if (operation === '/') {
-  answer = Number(firstNum) / Number(secondNum);
+
+let answer;
+switch (operation) {
+  case '+':
+    answer = Number(firstNum) + Number(secondNum);
+    break;
+  case '-':
+    answer = Number(firstNum) - Number(secondNum);
+    break;
+  case '*':
+    answer = Number(firstNum) * Number(secondNum);
+    break;
+  case '/':
+    answer = Number(firstNum) / Number(secondNum);
+    break;
 }
+
 // Print the result to the terminal.
 console.log(`${answer}`);
-console.log('Welcome to Calculator!');
